56023b5d3bf43f4ac94bff07198adc2916c98d62
Ondřej Súkup <osukup@suse.cz>
2017-09-05 09:17:47 +0200

    Update Copyright for test runners

--- kgr_tc_6.sh
+++ kgr_tc_6.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Test Case 3: Patch under cpu pressure
-# Patch a heavily hammered function in kernel
+
+# Copyright (C) 2017 SUSE
+# Authors: Lance Wang, Libor Pechacek
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Test Case 6: Patch while CPUs are busy
+# Unlike in TC 3 we are not invoking the patched function
 
 set -e
 . $(dirname $0)/kgr_tc_functions.sh
