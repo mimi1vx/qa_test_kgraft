mimi1vx/qa_test_kgraft

commit 589c77f730425c913db7c2b57789d5e64c4f09b8

Backport hiworkload from private sources
Original author: Lance Wang <lzwang@suse.com>

Backport of https://github.com/lpechacek/qa_test_klp/pull/5
(fix for https://github.com/lpechacek/qa_test_klp/issues/2).

Signed-off-by: Petr Vorel <pvorel@suse.cz>

kgr_tc_workload.sh
@@ -1,5 +1,5 @@
-# Copyright (C) 2017 SUSE
-# Authors: Lance Wang, Ondrej Sukup
+# Copyright (C) 2017-2019 SUSE
+# Authors: Lance Wang, Ondrej Sukup, Petr Vorel
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,19 +16,19 @@
 
 declare -a WORKLOAD_LIST
 function add_workload {
-    [ -z "$1" ]  && echo "WARNING: no parameters passed to $FUNCNAME"
+    [ -z "$1" ] && echo "WARNING: no parameters passed to $FUNCNAME"
     case $1 in
         cpu):;;
         mem):;;
-        *) echo "WARNING: $1 dose not exist";return 1;;
+        *) echo "WARNING: $1 does not exist"; return 1;;
     esac
     WORKLOAD_LIST[${#WORKLOAD_LIST[*]}]="$1"
 }
 
 function start_workload {
     for w in "${WORKLOAD_LIST[@]}"; do
-        echo "start ${w}"
-        eval "workload_$w > /dev/null"
+        echo "start $w"
+        eval "workload_$w"
     done
 }
 
@@ -51,17 +51,20 @@ function workload_cpu {
 
 function workload_mem {
     local pid
-    local CHIMEM_BIN='/usr/bin/chimem'
+    local chimem_bin="$SOURCE_DIR/hiworkload/src/chimem"
 
-    if [ -e "$SOURCE_DIR/bin/chimem" ]; then
-        CHIMEM_BIN="$SOURCE_DIR/bin/chimem"
+    if [ ! -x "$chimem_bin" ]; then
+        klp_tc_milestone "chimem binary is missing, compile it"
+        cd $SOURCE_DIR/hiworkload
+        ./compile.sh || klp_tc_abort "failed to compile chimem"
+        cd -
     fi
 
-    echo "WORKLOAD memory"
+    kgr_tc_milestone "WORKLOAD memory"
 
-    ${CHIMEM_BIN} &
+    $chimem_bin &
     pid=$!
     if test "X$(type -t push_recovery_fn)" == "Xfunction"; then
         push_recovery_fn "kill $pid"
-    fi    
+    fi
 }

hiworkload/.gitignore
@@ -1,0 +1,19 @@
+/aclocal.m4
+/autom4te.cache/
+/compile
+/config.h
+/config.h.in
+/config.log
+/config.status
+/configure
+/depcomp
+/install-sh
+/Makefile
+/Makefile.in
+/missing
+/src/.deps/
+/src/chimem
+/src/chimem.o
+/src/Makefile
+/src/Makefile.in
+/stamp-h1

hiworkload/Makefile.am
@@ -1,0 +1,4 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2017 SUSE
+# Author: Lance Wang <lzwang@suse.com>
+SUBDIRS = src

hiworkload/compile.sh
@@ -1,0 +1,11 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2019 Petr Vorel <pvorel@suse.cz>
+set -e
+
+aclocal
+autoconf
+autoheader
+automake -a
+./configure
+make -j$(getconf _NPROCESSORS_ONLN)

hiworkload/configure.ac
@@ -1,0 +1,10 @@
+dnl SPDX-License-Identifier: GPL-2.0-or-later
+dnl Copyright (C) 2017 SUSE
+dnl Author: Lance Wang <lzwang@suse.com>
+
+AC_INIT([hiworkload], [0.1], [lzwang@suse.com])
+AM_INIT_AUTOMAKE([foreign -Wall -Werror])
+AC_PROG_CC
+AC_CONFIG_HEADERS([config.h])
+AC_CONFIG_FILES([Makefile src/Makefile])
+AC_OUTPUT

hiworkload/src/Makefile.am
@@ -1,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2017 SUSE
+# Author: Lance Wang <lzwang@suse.com>
+bin_PROGRAMS = chimem
+chimem_SOURCES = chimem.c

hiworkload/src/chimem.c
@@ -1,0 +1,209 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2017 SUSE
+ * Author: Lance Wang <lzwang@suse.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+/* the work loop */
+static int wloop_goon;
+
+void
+wloop_reset(void)
+{
+    wloop_goon = 1;
+}
+
+void
+wloop_run(void)
+{
+    while (wloop_goon != 0) {
+        /* do nothing currently */
+        asm("nop" ::: "memory");
+    }
+}
+
+void
+wloop_stop(void)
+{
+    wloop_goon = 0;
+}
+
+/* signal */
+void
+sig_handler_term(int signum)
+{
+    wloop_stop();
+}
+
+int
+sig_install_handlers(void)
+{
+    int ret;
+    struct sigaction sa_term;
+
+    bzero(&sa_term, sizeof(sa_term));
+    sa_term.sa_handler = sig_handler_term;
+
+    ret = sigaction(SIGTERM, &sa_term, NULL);
+    return ret;
+}
+
+/* eat memory  */
+int
+mem_eat(size_t length)
+{
+    void *addr;
+
+    addr = mmap(NULL, length, PROT_READ | PROT_WRITE,
+                MAP_LOCKED | MAP_ANONYMOUS | MAP_PRIVATE,
+                -1, 0);
+
+    if (MAP_FAILED == addr) {
+        return -1;
+    }
+    return 0;
+}
+
+/* team */
+#define MIN_MEM_PER_WORKER 32 * 1024 * 1024
+#define WORKER_MAX 128
+pid_t worker_ID[WORKER_MAX];
+int worker_ID_I;
+
+void
+team_init(void)
+{
+    int i;
+
+    for (i = 0; i < WORKER_MAX; i++) {
+        worker_ID[i] = 0;
+    }
+    worker_ID_I = 0;
+}
+
+void
+team_add_worker(pid_t id)
+{
+    if (worker_ID_I < WORKER_MAX) {
+        worker_ID[worker_ID_I] = id;
+        worker_ID_I++;
+    } else {
+        fprintf(stderr, "TOO many workers!!!\n");
+    }
+}
+
+void
+team_kill_worker(void)
+{
+    int i;
+
+    for (i = 0; i < worker_ID_I; i++) {
+        kill(worker_ID[i], SIGTERM);
+    }
+}
+
+/* worker task */
+void
+worker (size_t length)
+{
+    int ret;
+
+    ret = mem_eat(length);
+    if (ret < 0) {
+        fprintf(stderr, "%lu failed alloc memory: %s\n",
+                getpid(), strerror(errno));
+        exit(5);
+    }
+    /* todo SET proc name */
+    wloop_reset();
+    wloop_run();
+    fprintf(stdout, "%lu finished.\n", getpid());
+}
+
+/* leader */
+int
+leader (int num_worker, size_t length_per_worker)
+{
+    int ret;
+    int i;
+
+    ret = sig_install_handlers();
+    if (ret < 0) {
+        fprintf(stderr, "%lu failed to install sig handlers\n",
+                getpid());
+        exit(22);
+    }
+
+    for (i = 0; i < num_worker; i++) {
+        ret = fork();
+        if (ret == 0) {
+            worker(length_per_worker);
+            exit(0);
+        } else if (ret < 0) {
+            fprintf(stderr, "FAILED to fork!\n");
+        } else {
+            int pid = ret;
+            team_add_worker(pid);
+        }
+    }
+
+    /* waiting */
+    wloop_reset();
+    wloop_run();
+
+    /* get childs */
+    team_kill_worker();
+    /* wait ?? */
+    exit(0);
+}
+
+/* main */
+int
+main (int argc, char **argv)
+{
+    size_t linelen = 0;
+    char *line = NULL;
+    unsigned long freemem = 0;
+    unsigned long lowmem = 0;
+    unsigned long mem_per_worker;
+    int worker_num;
+
+    FILE *f = fopen("/proc/meminfo", "r");
+    while (getdelim(&line, &linelen, '\n', f) > 0) {
+        sscanf(line, "LowFree: %lu", &lowmem);
+		sscanf(line, "MemFree: %lu", &freemem);
+    }
+	/* If system is configured with HIGH memory, use LowFree,
+	* else use MemFree */
+	if (lowmem) {
+		freemem = lowmem;
+	}
+
+    freemem *= 1024;
+
+    if (freemem < MIN_MEM_PER_WORKER) {
+        fprintf(stderr, "Memory is very low\n");
+        exit(1);
+    }
+
+    mem_per_worker = freemem / WORKER_MAX;
+    while (mem_per_worker < MIN_MEM_PER_WORKER) {
+        mem_per_worker *= 2;
+    }
+
+    worker_num = freemem / mem_per_worker;
+    printf("worker_num %d, mem_per_worker %luKib\n", worker_num, mem_per_worker / 1024);
+
+    leader(worker_num, mem_per_worker);
+    return 0;
+}

commit bd4c0d245d56f14aa43c11113f2ec5e817c6c9f2

Fix hiworkload backport
s/klp_/kgr_/

Fixes: 589c77f Backport hiworkload from private sources

Signed-off-by: Petr Vorel <pvorel@suse.cz>

kgr_tc_workload.sh
@@ -54,9 +54,9 @@ function workload_mem {
     local chimem_bin="$SOURCE_DIR/hiworkload/src/chimem"
 
     if [ ! -x "$chimem_bin" ]; then
-        klp_tc_milestone "chimem binary is missing, compile it"
+        kgr_tc_milestone "chimem binary is missing, compile it"
         cd $SOURCE_DIR/hiworkload
-        ./compile.sh || klp_tc_abort "failed to compile chimem"
+        ./compile.sh || kgr_tc_abort "failed to compile chimem"
         cd -
     fi
 